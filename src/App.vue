6644b515ea25f2fe8bc594e361b24daa8f08d25c
juliawel <julia20.wel@hotmail.com>
2024-10-03 17:43:24 -0300

    components

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <Header/>
+  <div class="container">
+    <Balance/>
+    <IncomeExpenses/>
+  </div>
+</template>
+
+<script>  
+  import Header from './components/Header.vue';
+  import Balance from './components/Balance.vue';
+  import IncomeExpenses from './components/IncomeExpenses.vue';
+
+  export default {
+    components: {
+      Header,
+      Balance,
+      IncomeExpenses,
+    }
+  }
+</script>
