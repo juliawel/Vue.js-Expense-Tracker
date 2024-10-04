8e3090e6e213d86a20372e0ff525ec9aeab84883
juliawel <julia20.wel@hotmail.com>
2024-10-04 09:09:19 -0300

    add transaction

---
+++ src/components/AddTransaction.vue
@@ -0,0 +1,16 @@
+<template>
+    <h3>Add a new transaction</h3>
+    <form id="form">
+        <div class="form-control">
+            <label for="text">Text</label>
+            <input type="text" id="text" placeholder="Enter a text...">
+        </div>
+        <div class="form-control">
+            <label for="amount">Amount 
+                <br/>(negative-expense, positive - income)
+            </label>
+            <input type="number" id="amount" placeholder="Enter amount...">
+        </div>
+        <button class="btn">Add transaction</button>
+    </form>
+</template>
